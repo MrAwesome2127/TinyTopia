5a425b0a4544e524eb1ca10a99ac9df6f720c136
Ryan Bednar <rbednar01@gmail.com>
2025-01-19 16:14:57 -0600

    Added express Server, Sequelize, and set up Bootstrap integration

---
+++ tiny-topia/src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+
+import sequelize from './config/connection.ts';
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+sequelize.sync().then(() => {
+  console.log(`Connected to database successfully.`);
+  app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+  });
+});
