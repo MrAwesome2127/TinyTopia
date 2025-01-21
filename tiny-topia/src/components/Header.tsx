faebd49d5ece6945f3eb794dfd4f144ca07b9a9c
Nebbs82 <nathan.r.ebbesen@hotmail.com>
2025-01-21 15:41:32 -0600

    Fixed a couple things, moved a couple things.

--- tiny-topia/src/components/Header.tsx
+++ tiny-topia/src/components/Header.tsx
@@ -1,27 +1,11 @@
 import '../styles/Header.css';
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const Header: React.FC = () => {
     return (
         <header className="bg-primary text-white text-center py-3">
-            <h1>Welcome to Our Landing Page</h1>
-            <nav>
-                <ul className="nav justify-content-center">
-                    <li className="nav-item">
-                        <Link className="nav-link text-white" to="/">Home</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link text-white" to="/models">Models</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link text-white" to="/perks">Perks</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link text-white" to="/faq">FAQ</Link>
-                    </li>
-                </ul>
-            </nav>
+            <h1>Welcome to TinyTopia!</h1>
+            <p>Where the world is small and the possibilities are endless.</p>
         </header>
     );
 };
--- tiny-topia/src/pages/LandingPage.tsx
+++ tiny-topia/src/pages/LandingPage.tsx
@@ -1,11 +1,28 @@
 import '../styles/LandingPage.css';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-
-const LandingPage = () => {
+const LandingPage: React.FC = () => {
     return (
         <div>
         <h1>Welcome to TinyTopia</h1>
         <p>Where the world is small and the possibilities are endless.</p>
+        <nav>
+            <ul className="nav justify-content-center">
+                <li className="nav-item">
+                    <Link className="nav-link text-white" to="/">Home</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link text-white" to="/models">Models</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link text-white" to="/perks">Perks</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link text-white" to="/faq">FAQ</Link>
+                </li>
+            </ul>
+        </nav>
         </div>
     );
 };
