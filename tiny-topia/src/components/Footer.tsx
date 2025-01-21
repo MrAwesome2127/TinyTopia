bf7c39c4812d4b854032269e9c8c6845482766b6
Nebbs82 <nathan.r.ebbesen@hotmail.com>
2025-01-20 19:09:27 -0600

    added and changed a few elements, need consult.

--- tiny-topia/src/components/Footer.tsx
+++ tiny-topia/src/components/Footer.tsx
@@ -1,11 +1,12 @@
 import '../styles/Footer.css';
+import React from 'react';
 
-const Footer = () => {
+const Footer: React.FC = () => {
     return (
-        <footer>
-         <p> 2021 TinyTopia</p>
+        <footer className="bg-dark text-white text-center py-3">
+            <p>&copy; 2025 TinyTopia. All Rights Reserved.</p>
         </footer>
     );
-}
+};
 
 export default Footer;
--- tiny-topia/src/components/Header.tsx
+++ tiny-topia/src/components/Header.tsx
@@ -1,10 +1,29 @@
 import '../styles/Header.css';
-const Header = () => {
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header: React.FC = () => {
     return (
-        <header>
-            <h1>Tiny Topia</h1>
+        <header className="bg-primary text-white text-center py-3">
+            <h1>Welcome to Our Landing Page</h1>
+            <nav>
+                <ul className="nav justify-content-center">
+                    <li className="nav-item">
+                        <Link className="nav-link text-white" to="/">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link text-white" to="/models">Models</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link text-white" to="/perks">Perks</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link text-white" to="/faq">FAQ</Link>
+                    </li>
+                </ul>
+            </nav>
         </header>
     );
-}
+};
 
 export default Header;
