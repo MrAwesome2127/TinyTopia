bf0e6ff872d90083e60e72603616242ef50d184c
Nebbs82 <nathan.r.ebbesen@hotmail.com>
2025-01-23 17:47:31 -0600

    Landing Page done ..I think

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,33 @@
-import './App.css'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import {Outlet} from 'react-router-dom'
-
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import FAQ from './pages/FAQ';
+import Perks from './pages/Perks';
+import LandingPage from './pages/LandingPage';
+import AboutUs from './pages/AboutUs';
+import ContactUs from './pages/ContactUs';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import WelcomeSection from './pages/Welcome-Section';
 
 function App() {
   return (
-    <div>
+    <Router>
       <Header />
-        <Outlet />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="welcome" element={<WelcomeSection />} />
+          <Route path="about" element={<AboutUs />} />
+          <Route path="contact" element={<ContactUs />} />
+          <Route path="login" element={<Login />} />
+          <Route path="faq" element={<FAQ />} />
+          <Route path="perks" element={<Perks />} />
+        </Routes>
+      </div>
       <Footer />
-    </div>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
