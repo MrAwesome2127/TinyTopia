8cf80981cc9edefd23e8a8538e100682c72c85bf
Tim Velasquez <Timmyv1031@gmail.com>
2025-01-28 18:52:55 -0600

    Added about us content

--- client/src/pages/AboutUs.tsx
+++ client/src/pages/AboutUs.tsx
@@ -1,28 +1,216 @@
+import React from "react";
 
 const AboutUs = () => {
-    return (
-        <div>
-        <h1>About Us</h1>
-        <p>
-        Welcome to TinyTopia, where we believe in big dreams and small footprints!!
-
-        At TinyTopia, we are passionate about the tiny home movement and dedicated to providing an exceptional experience for those seeking a unique and sustainable lifestyle.
-        Our company specializes in the sales, manufacture, service, and maintenance of tiny homes, ensuring that our clients receive a comprehensive and tailored solution for their compact living dreams.
-
-        Our mission is to create a family-friendly environment where individuals, couples, and families can embrace a simpler, more eco-friendly way of life. 
-        We understand that choosing a tiny home is not just about downsizing; it's about making a conscious decision to reduce your environmental impact while maximizing comfort and functionality.
-
-        In our manufacturing process, we prioritize the use of sustainable materials and innovative design techniques to create homes that are not only aesthetically pleasing but also energy-efficient. 
-        From the initial design consultation to the final touches, we work closely with our clients to bring their vision to life, ensuring every tiny home is a reflection of their unique style and needs.
-
-        But our commitment doesn't end with the sale. TinyTopia offers ongoing service and maintenance support, ensuring that your tiny home remains a safe and comfortable haven for years to come. 
-        Our team of experts is dedicated to providing prompt and reliable assistance, allowing you to focus on creating unforgettable memories in your new, cozy space.
-
-        Whether you're a nature enthusiast, a minimalist, or a family seeking a unique adventure, TinyTopia is here to guide you every step of the way. 
-        Join us on this journey towards a more sustainable future, one tiny home at a time!
+  return (
+    <div className="container mt-5">
+      <h1 className="text-center mb-4">About Us</h1>
+      <p className="lead">
+        Welcome to TinyTopia, where we believe in big dreams and small
+        footprints!! 
+        <br />
+        At TinyTopia, we are passionate about the tiny home movement and
+        dedicated to providing an exceptional experience for those seeking a
+        unique and sustainable lifestyle. Our company specializes in the sales,
+        manufacture, service, and maintenance of tiny homes, ensuring that our
+        clients receive a comprehensive and tailored solution for their compact
+        living dreams.
+        <br />
+        Our mission is to create a family-friendly environment where
+        individuals, couples, and families can embrace a simpler, more
+        eco-friendly way of life. We understand that choosing a tiny home is not
+        just about downsizing; it's about making a conscious decision to reduce
+        your environmental impact while maximizing comfort and functionality. In
+        our manufacturing process, we prioritize the use of sustainable
+        materials and innovative design techniques to create homes that are not
+        only aesthetically pleasing but also energy-efficient. From the initial
+        design consultation to the final touches, we work closely with our
+        clients to bring their vision to life, ensuring every tiny home is a
+        reflection of their unique style and needs. 
+        <br />
+        But our commitment doesn't end with the sale. TinyTopia offers ongoing
+        service and maintenance support, ensuring that your tiny home remains a
+        safe and comfortable haven for years to come. Our team of experts is
+        dedicated to providing prompt and reliable assistance, allowing you to
+        focus on creating unforgettable memories in your new, cozy space. <br />
+        Whether you're a nature enthusiast, a minimalist, or a family seeking a
+        unique adventure, TinyTopia is here to guide you every step of the way.{" "}
+        <br />
+        Join us on this journey towards a more sustainable future, one tiny home
+        at a time!
+        <br />
+      </p>
+    </div>
+    
+    <hr class="featurette-divider"></hr>
+    <div class="row featurette">
+      <div class="col-md-7">
+        <h2 class="featurette-heading fw-normal lh-1">
+            <font style="vertical-align: inherit;">
+                <font style="vertical-align: inherit;">Tim Velasquez </font>
+            </font>
+            <span class="text-body-secondary">
+                <font style="vertical-align: inherit;">
+                    <font style="vertical-align: inherit;">President CEO </font>
+                </font>
+            </span>
+        </h2>
+        <p class="lead">
+            <font style="vertical-align: inherit;">
+                <font style="vertical-align: inherit;">The person who makes decisions that is typically wrong.</font>
+            </font>
         </p>
-        </div>
-    );
-}
+      </div>
+      <div class="col-md-5">
+        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false">
+            <title>Placeholder</title>
+            <rect width="100%" height="100%" fill="var(--bs-secondary-bg)"></rect>
+            <text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">500x500</text>
+        </svg>
+      </div>
+    </div>
+
+    <hr class="featurette-divider"></hr>
+    <div class="row featurette">
+      <div class="col-md-7">
+        <h2 class="featurette-heading fw-normal lh-1">
+            <font style="vertical-align: inherit;">
+                <font style="vertical-align: inherit;">Nathan Ebbesen </font>
+            </font>
+            <span class="text-body-secondary">
+                <font style="vertical-align: inherit;">
+                    <font style="vertical-align: inherit;">Master Tiny Designer</font>
+                </font>
+            </span>
+        </h2>
+        <p class="lead">
+            <font style="vertical-align: inherit;">
+                <font style="vertical-align: inherit;">The Bob Ross of tiny homes who makes a friend next to the tree. </font>
+            </font>
+        </p>
+      </div>
+      <div class="col-md-5">
+        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false">
+            <title>Placeholder</title>
+            <rect width="100%" height="100%" fill="var(--bs-secondary-bg)"></rect>
+            <text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">500x500</text>
+        </svg>
+      </div>
+    </div>
+
+    <hr class="featurette-divider"></hr>
+    <div class="row featurette">
+      <div class="col-md-7">
+        <h2 class="featurette-heading fw-normal lh-1">
+            <font style="vertical-align: inherit;">
+                <font style="vertical-align: inherit;">Ryan Bednar </font>
+            </font>
+            <span class="text-body-secondary">
+                <font style="vertical-align: inherit;">
+                    <font style="vertical-align: inherit;">Master Tiny Developer </font>
+                </font>
+            </span>
+        </h2>
+        <p class="lead">
+            <font style="vertical-align: inherit;">
+                <font style="vertical-align: inherit;">Person who codes and says things that make it sound like Bugs live in the software </font>
+            </font>
+        </p>
+      </div>
+      <div class="col-md-5">
+        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false">
+            <title>Placeholder</title>
+            <rect width="100%" height="100%" fill="var(--bs-secondary-bg)"></rect>
+            <text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">500x500</text>
+        </svg>
+      </div>
+    </div>
+
+    <hr class="featurette-divider"></hr>
+    <div class="row featurette">
+      <div class="col-md-7">
+        <h2 class="featurette-heading fw-normal lh-1">
+            <font style="vertical-align: inherit;">
+                <font style="vertical-align: inherit;">Kassandra Peasley </font>
+            </font>
+            <span class="text-body-secondary">
+                <font style="vertical-align: inherit;">
+                    <font style="vertical-align: inherit;">IT Developer </font>
+                </font>
+            </span>
+        </h2>
+        <p class="lead">
+            <font style="vertical-align: inherit;">
+                <font style="vertical-align: inherit;">Person who takes the code and steps on bugs to make it fool proof.</font>
+            </font>
+        </p>
+      </div>
+      <div class="col-md-5">
+        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false">
+            <title>Placeholder</title>
+            <rect width="100%" height="100%" fill="var(--bs-secondary-bg)"></rect>
+            <text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">500x500</text>
+        </svg>
+      </div>
+    </div>
+
+    <hr class="featurette-divider"></hr>
+    <div class="row featurette">
+      <div class="col-md-7">
+        <h2 class="featurette-heading fw-normal lh-1">
+            <font style="vertical-align: inherit;">
+                <font style="vertical-align: inherit;">Levi Frandsen </font>
+            </font>
+            <span class="text-body-secondary">
+                <font style="vertical-align: inherit;">
+                    <font style="vertical-align: inherit;">IT Architect </font>
+                </font>
+            </span>
+        </h2>
+        <p class="lead">
+            <font style="vertical-align: inherit;">
+                <font style="vertical-align: inherit;">Designer of Product to make the Service look like masterpiece that only conflicts with the developers.</font>
+            </font>
+        </p>
+      </div>
+      <div class="col-md-5">
+        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false">
+            <title>Placeholder</title>
+            <rect width="100%" height="100%" fill="var(--bs-secondary-bg)"></rect>
+            <text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">500x500</text>
+        </svg>
+      </div>
+    </div>
+
+    <hr class="featurette-divider"></hr>
+    <div class="row featurette">
+      <div class="col-md-7">
+        <h2 class="featurette-heading fw-normal lh-1">
+            <font style="vertical-align: inherit;">
+                <font style="vertical-align: inherit;">Matt Unrein </font>
+            </font>
+            <span class="text-body-secondary">
+                <font style="vertical-align: inherit;">
+                    <font style="vertical-align: inherit;">IT Logo Designer</font>
+                </font>
+            </span>
+        </h2>
+        <p class="lead">
+            <font style="vertical-align: inherit;">
+                <font style="vertical-align: inherit;">It was easy to do.</font>
+            </font>
+        </p>
+      </div>
+      <div class="col-md-5">
+        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false">
+            <title>Placeholder</title>
+            <rect width="100%" height="100%" fill="var(--bs-secondary-bg)"></rect>
+            <text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">500x500</text>
+        </svg>
+      </div>
+    </div>
+
+    <hr class="featurette-divider"></hr>
+  );
+};
 
 export default AboutUs;
